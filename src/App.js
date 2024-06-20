8c59a01fe74419afae69330a805f930b0e76345f
Kanan Guluzade <kanan.guluzade@abb-bank.az>
2024-06-20 12:05:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Input } from "./components/Input";
+import React, { useState } from "react";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input taskList={taskList} setTaskList={setTaskList} />
+      {taskList.map((item) => {
+        return (
+          <ul>
+            <li>{item}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+export const Input = ({ taskList, setTaskList }) => {
+  const [input, setInput] = useState("");
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
+
+  const handleChange = (value) => {
+    setInput(value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    setTaskList([input, ...taskList]);
+    setInput("");
+  };
+
+  const handleSelectAll = () => {
+    if (selectAll) {
+      setSelectedItems([]);
+    } else {
+      const allItems = [1, 2, 3];
+      setSelectedItems(allItems);
+    }
+    setSelectAll(!selectAll);
+  };
+
+  const handleSelectItem = (id) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((item) => item !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]); // Öğeyi seç
+    }
+  };
+
+  // OBJECTS
+  const person = {};
+  // console.log("person", person);
+  const person2 = {
+    name: ["kenan", "ramal"],
+    age: "32",
+    bio: function () {
+      console.log(`${this.name} is ${this.age} old`);
+    },
+  };
+  console.log(person2.age);
+  console.log(person2.name);
+  console.log(person2.bio());
+
+  return (
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="todo"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <button onClick={handleAddTask}>add</button>
+        <button type="submit">submit</button> <br /> <br />
+        <button>click</button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input
+                type="checkbox"
+                checked={selectAll}
+                onClick={handleSelectAll}
+              />
+            </th>
+            <th>Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[1, 2, 3].map((id) => (
+            <tr key={id}>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={selectedItems.includes(id)}
+                  onChange={() => handleSelectItem(id)}
+                />
+              </td>
+              <td>Item {id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
